ca3515472fabcca148ef1e636ccf8eec4606c4bc
Dionisia Montanez <dmontanez15@gmail.com>
2018-11-06 15:50:57 -0800

    movies controllers and tests passing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,39 @@
 class MoviesController < ApplicationController
+  def index
+    movies = Movie.all
+    render json: jsonify(movies), status: :ok #response code
+  end
+
+  def show
+    movie = Movie.find_by(id: params[:id])
+
+    if movie
+      render json: jsonify(movie), status: :ok #response code
+    else
+      # options
+      # head :not_found
+      # render json: {}, status: :not_found
+      render_error(:not_found, { movie_id: ["No such movie"] } )
+    end
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+    # binding.pry
+    if movie.save
+      render json: { id: movie.id }, status: :ok
+    else
+      render_error(:bad_request, movie.errors.messages)
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:id, :title, :overview, :release_date, :inventory)
+  end
+
+  def jsonify(movie_data)
+    return movie_data.as_json(only: [:id, :title, :overview, :release_date, :inventory])
+  end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,89 @@
 require "test_helper"
 
 describe MoviesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+# it "must be a real test" do
+#   flunk "Need real tests"
+# end
+MOVIE_FIELDS = %w(id title overview release_date inventory).sort
+
+def parse_json(expected_type:, expected_status: :success)
+  must_respond_with expected_status
+  expect(response.header['Content-Type']).must_include 'json'
+
+  body = JSON.parse(response.body)
+  expect(body).must_be_kind_of expected_type
+  return body
+end
+
+describe "index" do
+  it "is a working route" do
+    get movies_path
+
+    body = parse_json(expected_type: Array)
+    expect(body.length).must_equal Movie.count
+  end
+
+  it "returns and empty array when there are no movies" do
+    Movie.destroy_all
+    get movies_path
+    body = parse_json(expected_type: Array)
+    expect(body.length).must_equal 0
+  end
+end
+
+describe "show" do
+  it "returns a movie with exactly the required fields" do
+    movie = Movie.first
+
+    get movie_path(movie)
+    # binding.pry
+    body = parse_json(expected_type: Hash)
+    expect(body.keys.sort).must_equal MOVIE_FIELDS
+  end
+
+  it "doesn't blow up when a movie DNE" do
+    movie = Movie.first
+    movie.destroy
+
+    get movie_path(movie)
+
+    body = parse_json(expected_type: Hash, expected_status: :not_found)
+    expect(body).must_include "errors"
+  end
+end
+
+describe "create" do
+  before do
+    @movie = {
+      title: "The Great Escape",
+      overview: "Movie about WWII",
+      release_date: "1965-01-18",
+      inventory: 10
+    }
+  end
+
+  it "creates a new movie given valid data" do
+    expect{
+      post movies_path, params: { movie: @movie } }.must_change "Movie.count", 1
+      must_respond_with :success
+
+      body = parse_json(expected_type: Hash)
+      expect(body).must_include "id"
+    end
+  end
+
+  it "returns an error for invalid data" do
+    @movie = {
+      title: nil
+    }
+
+    expect{
+      post movies_path, params: { movie: @movie } }.wont_change "Customer.count"
+      # binding.pry
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "title"
+      must_respond_with :bad_request
+  end
 end
