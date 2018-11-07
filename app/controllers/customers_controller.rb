38a604b26965a49b898def032cb0728eff9a5e18
Karis Kim <ouniiq@hotmail.com>
2018-11-07 10:03:02 -0800

    changed fields to return to match requirement

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,36 +4,37 @@ class CustomersController < ApplicationController
   def index
     customers = Customer.all
     render json: jsonify(customers), status: :ok #response code
-  end
-
-  def show
-    customer = Customer.find_by(id: params[:id])
-
-    if customer
-      render json: jsonify(customer), status: :ok #response code
-    else
-      # options
-      # head :not_found
-      # render json: {}, status: :not_found
-      render_error(:not_found, { customer_id: ["No such customer"] } )
-    end
-  end
-
-  def create
-    customer = Customer.new(customer_params)
     # binding.pry
-    if customer.save
-      render json: { id: customer.id }, status: :ok
-    else
-      render_error(:bad_request, customer.errors.messages)
-    end
   end
+
+  # def show
+  #   customer = Customer.find_by(id: params[:id])
+  #
+  #   if customer
+  #     render json: jsonify(customer), status: :ok #response code
+  #   else
+  #     # options
+  #     # head :not_found
+  #     # render json: {}, status: :not_found
+  #     render_error(:not_found, { customer_id: ["No such customer"] } )
+  #   end
+  # end
+
+  # def create
+  #   customer = Customer.new(customer_params)
+  #   # binding.pry
+  #   if customer.save
+  #     render json: { id: customer.id }, status: :ok
+  #   else
+  #     render_error(:bad_request, customer.errors.messages)
+  #   end
+  # end
 
   private
 
   def customer_params
     # binding.pry
-    params.require(:customer).permit(:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count)
+    params.require(:customer).permit(:name, :registered_at, :postal_code, :phone, :movies_checked_out_count)
   end
 
   def jsonify(customer_data)
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -33,63 +33,63 @@ describe CustomersController do
     end
   end
 
-  describe "show" do
-    it "returns a customer with exactly the required fields" do
-      customer = Customer.first
+  # describe "show" do
+  #   it "returns a customer with exactly the required fields" do
+  #     customer = Customer.first
+  #
+  #     get customer_path(customer)
+  #     # binding.pry
+  #     body = parse_json(expected_type: Hash)
+  #     expect(body.keys.sort).must_equal CUSTOMER_FIELDS
+  #   end
+  #
+  #   it "doesn't blow up when a customer DNE" do
+  #     customer = Customer.first
+  #     customer.destroy
+  #
+  #     get customer_path(customer)
+  #
+  #     body = parse_json(expected_type: Hash, expected_status: :not_found)
+  #     expect(body).must_include "errors"
+  #   end
+  # end
 
-      get customer_path(customer)
-      # binding.pry
-      body = parse_json(expected_type: Hash)
-      expect(body.keys.sort).must_equal CUSTOMER_FIELDS
-    end
-
-    it "doesn't blow up when a customer DNE" do
-      customer = Customer.first
-      customer.destroy
-
-      get customer_path(customer)
-
-      body = parse_json(expected_type: Hash, expected_status: :not_found)
-      expect(body).must_include "errors"
-    end
-  end
-
-  describe "create" do
-    before do
-      date = DateTime.now
-      @customer = {
-        name: "Ada",
-        registered_at: date,
-        address: "123 River Road",
-        city: "Seattle",
-        state: "WA",
-        postal_code: "98155",
-        phone: "206-421-9876"
-      }
-    end
-
-    it "creates a new customer given valid data" do
-      expect{
-        post customers_path, params: { customer: @customer } }.must_change "Customer.count", 1
-      must_respond_with :success
-
-      body = parse_json(expected_type: Hash)
-      expect(body).must_include "id"
-    end
-
-    it "returns an error for invalid data" do
-      @customer = {
-        name: nil
-      }
-
-      expect{
-        post customers_path, params: { customer: @customer } }.wont_change "Customer.count"
-        # binding.pry
-        body = JSON.parse(response.body)
-        expect(body).must_be_kind_of Hash
-        expect(body).must_include "errors"
-        expect(body["errors"]).must_include "name"
-        must_respond_with :bad_request
-      end
-  end
+  # describe "create" do
+  #   before do
+  #     date = DateTime.now
+  #     @customer = {
+  #       name: "Ada",
+  #       registered_at: date,
+  #       address: "123 River Road",
+  #       city: "Seattle",
+  #       state: "WA",
+  #       postal_code: "98155",
+  #       phone: "206-421-9876"
+  #     }
+  #   end
+  #
+  #   it "creates a new customer given valid data" do
+  #     expect{
+  #       post customers_path, params: { customer: @customer } }.must_change "Customer.count", 1
+  #     must_respond_with :success
+  #
+  #     body = parse_json(expected_type: Hash)
+  #     expect(body).must_include "id"
+  #   end
+  #
+  #   it "returns an error for invalid data" do
+  #     @customer = {
+  #       name: nil
+  #     }
+  #
+  #     expect{
+  #       post customers_path, params: { customer: @customer } }.wont_change "Customer.count"
+  #       # binding.pry
+  #       body = JSON.parse(response.body)
+  #       expect(body).must_be_kind_of Hash
+  #       expect(body).must_include "errors"
+  #       expect(body["errors"]).must_include "name"
+  #       must_respond_with :bad_request
+  #     end
+  # end
 end
