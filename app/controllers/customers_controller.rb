4faaf7fb48ece8cbf8189537d0c43c347669dc32
Karis Kim <ouniiq@hotmail.com>
2018-11-06 16:45:47 -0800

    added movies_checked_out via migration and added more validation to customers and added tests for those validations

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -32,10 +32,10 @@ class CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone)
+    params.require(:customer).permit(:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count)
   end
 
   def jsonify(customer_data)
-    return customer_data.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone])
+    return customer_data.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone, :movies_checked_out_count])
   end
 end
